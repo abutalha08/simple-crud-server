0eaa96bca3ea7357f5120293833960602c90e1de
abutalha08 <taufique752@gmail.com>
2026-05-13 22:40:25 +0600

    Database setup in mongoDB and Connection logic, async await and try catch block

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://simpleCrudUser:NQ8t61uoCQZQimDw@cluster0.eae5sfq.mongodb.net/?appName=Cluster0`;
+
+// simpleCrudUser
+
+// NQ8t61uoCQZQimDw
+
+// mongodb+srv://<db_username>:<db_password>@cluster0.eae5sfq.mongodb.net/?appName=Cluster0
+
+// mongodb+srv://simpleCrudUser:NQ8t61uoCQZQimDw@cluster0.eae5sfq.mongodb.net/?appName=Cluster0
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const run = async () => {
+    try{ 
+        await client.connect();
+        await client.db('admin').command({ ping: 1});
+        console.log("Pinged your deployment ..You successfully connected mongoDB");
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Simple CRUD server is running')
